318f1251d751fb8fc0e7f0a2415cc1a3f034ee68
max <maxsteffens@gmail.com>
2021-11-20 16:13:44 -0300

    funcional

--- TPIM8a/app/src/main/java/br/com/unip/tpim8a/MainActivity.java
+++ TPIM8a/app/src/main/java/br/com/unip/tpim8a/MainActivity.java
@@ -2,6 +2,10 @@ package br.com.unip.tpim8a;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void enviar(View view){
+        TextInputEditText campoNome = findViewById(R.id.editNome);
+        TextInputEditText campoCPF = findViewById(R.id.editCPF);
+        TextView textoResultado = findViewById(R.id.textResultado);
+        String nome = campoNome.getText().toString();
+        String cpf = campoCPF.getText().toString();
+
+        textoResultado.setText("Nome:"+nome+ "\n CPF"+cpf);
+
+
     }
 }
